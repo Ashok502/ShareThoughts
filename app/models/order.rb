887657579e6c7e75767a9cf99f6374769a9faf33
rails <rails@rails.com>
2015-09-11 11:56:46 +0530

    write the contast for keys

--- app/models/order.rb
+++ app/models/order.rb
@@ -19,7 +19,7 @@ class Order < ActiveRecord::Base
   def express_token=(token)
     self[:express_token] = token
     if new_record? && !token.blank?
-      details = EXPRESS_GATEWAY.details_for(token)
+      details = EXPRESS.details_for(token)
       self.express_payer_id = details.payer_id
       self.first_name = details.params["first_name"]
       self.last_name = details.params["last_name"]
