eddc20ba79de398c483f447dc24650309dc583cd
rails <rails@rails.com>
2015-10-08 16:22:23 +0530

    video name

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :images, :as => :imageable
-  has_many :videos, :as => :videoable
+  has_many :share_videos, :as => :videoable
   has_many :line_items
   accepts_nested_attributes_for :images, :allow_destroy => true, :reject_if => :all_blank
 
